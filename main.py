41a1d805d5da6600d3f9df5064a1495deed5a8e8
Yas-min <yasmin.senaysb@gmail.com>
2023-03-02 18:13:47 -0300

    v0.2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Texto'), sg.Spin(['item 1', 'item 2'])],
-    [sg.Button('Botão')], 
-    [sg.Input()]
+    [sg.Text('Text', enable_events = True, key = '-TEXT-'), sg.Spin(['item 1', 'item 2'])],
+    [sg.Button('Botão', key = '-BUTTON1-')], 
+    [sg.Input()],
+    [sg.Text('Teste'), sg.Button('Botão de Teste', key = '-BUTTON2-')]
 ]
 
 window = sg.Window('Conversor', layout)
@@ -11,10 +12,27 @@ window = sg.Window('Conversor', layout)
 while True:
     event, values = window.read()
 
-    if event == sg.WIN_CLOSED:
-        break
+    match event:
+        case sg.WIN_CLOSED:
+            break
 
-    if event == '-BUTTON1':
-        print('button pressed')
+        case '-BUTTON1-':
+            print(values[1])
+
+        case '-BUTTON2-':
+            print('Test')
+
+        case '-TEXT-':
+            print('Text was pressed')
+
+    # match values[0]:
+    #     case 1:
+    #         text1 = 'item 1'
+
+    #     case 2:
+    #         text1 = 'item 2'
+
+
+
 
 window.close()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     

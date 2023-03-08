a1f695ebf20fc4f1ee95a2212701f45dfe609dd1
Yas-min <yasmin.senaysb@gmail.com>
2023-03-08 15:51:32 -0300

    v.1

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Text', enable_events = True, key = '-TEXT-'), sg.Spin(['item 1', 'item 2'])],
-    [sg.Button('Botão', key = '-BUTTON1-')], 
-    [sg.Input()],
-    [sg.Text('Teste'), sg.Button('Botão de Teste', key = '-BUTTON2-')]
+    [
+        sg.Input(key = '-INPUT-'),
+        sg.Spin(['km to mile', 'kg to pound', 'sec to min'], key = '-UNITS-'),
+        sg.Button('Convert', key = '-CONVERT-')
+    ],
+    [sg.Text('Conversion', key = '-OUTPUT-')]
 ]
 
-window = sg.Window('Conversor', layout)
+window = sg.Window('Converter', layout)
 
 while True:
     event, values = window.read()
 
     match event:
+
         case sg.WIN_CLOSED:
             break
 
-        case '-BUTTON1-':
-            print(values[1])
+        case '-CONVERT-':
+            input_value = values['-INPUT-']
+            if input_value.isnumeric():
+                print(input_value)
+                match values['-UNITS-']:
+                    case 'km to mile':
+                        output = round(float(input_value) * 0.06214)
+                        output_string = f'{input_value} km are {output} miles.'
 
-        case '-BUTTON2-':
-            print('Test')
+                    case 'kg to pound':
+                        output = round(float(input_value) * 2.20462)
+                        output_string = f'{input_value} kg are {output} pounds.'
 
-        case '-TEXT-':
-            print('Text was pressed')
+                    case 'sec to min':
+                        output = round(float(input_value)/60)
+                        output_string = f'{input_value} seconds are {output} minutes'
+
+                window['-OUTPUT-'].update(output_string)
+
+
+
+    # match event:
+    #     case sg.WIN_CLOSED:
+    #         break
+    #
+    #     case '-BUTTON1-':
+    #         print(values[1])
+    #
+    #     case '-BUTTON2-':
+    #         print('Test')
+    #
+    #     case '-TEXT-':
+    #         print('Text was pressed')
 
     # match values[0]:
     #     case 1:
@@ -32,7 +60,4 @@ while True:
     #     case 2:
     #         text1 = 'item 2'
 
-
-
-
-window.close()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+window.close()
